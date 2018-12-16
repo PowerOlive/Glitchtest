1a1e2b659079cccd7bf5a11c70a08f3f7eed3e50
James Stevenson <jastevenson303@gmail.com>
2018-12-15 20:15:00 -0500

    Note book reader node/mod.

---
+++ mods/books/init.lua
@@ -0,0 +1,17 @@
+-- `books' mod for Minetest game `Glitchtest'
+-- Copyright 2018 James Alexander Stevenson
+-- GNU GPL 3
+
+-- A node that stores various written things, in particular books.
+-- A "book reader" node, that shows an interface to its contents,
+-- paginated, with tabs, for easy reading among multiple books.
+
+-- I may incorporate Glitchtest specific books into this mod, for
+-- distribution with the game.  A sort of "in-game help system."
+
+-- Ideally these books would be generated from a flat text file.
+-- Then, perhaps, the "?" `help' dialogs could also pull from there.
+
+-- I'd also like to rehash my old `books' mod, "Placeable Books."
+-- Then, I would move jas0/paper.lua as well as default:*book*,
+-- which includes the shelf.
