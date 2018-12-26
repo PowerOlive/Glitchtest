3356662afa490db6ede4dcd3b5a5365296563c53
James Stevenson <jastevenson303@gmail.com>
2018-12-25 23:35:53 -0500

    Add antiflame.

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -1,3 +1,7 @@
+-- This `fire' mod is part of Glitchtest game
+-- Copyright 2018 James Stevenson
+-- GNU GPL 3
+
 -- Global namespace for functions
 
 fire = {}
@@ -11,6 +15,15 @@ end
 --
 
 -- Flood flame function
+local function on_punch(pos, node, puncher, pointed_thing)
+	if puncher:get_wielded_item():get_name() == "default:mese_crystal" then
+		local n = minetest.find_node_near(pos, 1, {"fire:basic_flame"})
+		if n then
+			minetest.set_node(n, {name = "fire:antiflame"})
+		end
+		minetest.set_node(pointed_thing.under, {name = "fire:antiflame"})
+	end
+end
 
 local function flood_flame(pos, oldnode, newnode)
 	-- Play flame extinguish sound if liquid is not an 'igniter'
@@ -25,6 +38,30 @@ local function flood_flame(pos, oldnode, newnode)
 end
 
 -- Flame nodes
+
+minetest.register_node("fire:antiflame", {
+	drawtype = "firelike",
+	tiles = {
+		{
+			name = "fire_basic_flame_animated.png^[colorize:cyan:127",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 1,
+			},
+		},
+	},
+	inventory_image = "fire_basic_flame.png^[colorize:cyan:127",
+	drop = "",
+	paramtype = "light",
+	light_source = 14,
+	walkable = false,
+	buildable_to = true,
+	sunlight_propagates = true,
+	damage_per_second = 4,
+	groups = {dig_immediate = 3, not_in_creative_inventory = 1},
+})
 
 minetest.register_node("fire:basic_flame", {
 	drawtype = "firelike",
@@ -63,7 +100,7 @@ minetest.register_node("fire:basic_flame", {
 	on_construct = function(pos)
 		minetest.get_node_timer(pos):start(math.random(30, 60))
 	end,
-
+	on_punch = on_punch,
 	on_flood = flood_flame,
 })
 
@@ -91,7 +128,7 @@ minetest.register_node("fire:permanent_flame", {
 	damage_per_second = 4,
 	groups = {igniter = 2, dig_immediate = 3},
 	drop = "",
-
+	on_punch = on_punch,
 	on_flood = flood_flame,
 })
 
@@ -336,4 +373,18 @@ minetest.register_abm({
 			minetest.remove_node(pos)
 		end
 	end,
+})
+
+minetest.register_abm({
+	nodenames = {"fire:antiflame"},
+	interval = 2,
+	chance = 1,
+	catch_up = false,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local n = minetest.find_node_near(pos, 5, {"fire:basic_flame"})
+		if n then
+			return minetest.set_node(n, {name = "fire:antiflame"})
+		end
+		minetest.remove_node(pos)
+	end
 })
